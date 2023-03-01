de16fd91679a0f87e25631cc228c535c17e0a967
Aditya sharma <adityaraysharma@gmail.com>
2023-03-01 19:23:57 +0530

    created routes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
-
+import React,{useState,useEffect} from 'react'
+import {Loader,Card,FormField} from '../components'
 const Home = () => {
+  const [loading,setLoading]=useState(False)
+  const [allPosts,setAllPosts]=useState(null);
   return (
-    <div>Home</div>
+   <section className="max-w-7xl mx-auto">
+    <div>
+      <h1 className="font-extrabold text-[#22328] text-[32px]">
+        The Community Showcase
+      </h1>
+      <p classNmae="mt-2 text-[#666e75]"></p>
+    </div>
+   </section>
   )
 }
 
