53a479d51755329ce4172d16ca06ff46fa01f5ef
Aditya sharma <adityaraysharma@gmail.com>
2023-03-09 23:55:25 +0530

    reverting
    This reverts commit eaf2b867371456d2f4b8479524084b0dbfe8e2dc.
    
    git commit -m "revert"

--- server/index.js
+++ server/index.js
@@ -8,8 +8,8 @@ dotenv.config();
 const app=express();
 app.use(cors());
 app.use(express.json({limit:'50mb'}));
-app.use('/api/v1/post',postRoutes);
-app.use('/api/v1/dalle',dalleRoutes);
+app.use('api/v1/post',PostRoutes)
+app.use('api,dalle/v1/',dalle)
 app.get('/',async(req,res)=>{
     res.send('hello from DALL-E')
 })
@@ -18,6 +18,8 @@ const startServer=async ()=>{
     
     try{
       connectDB(process.env.MONGODB_URL);
+    
+
     app.listen(8080,()=>console.log('server has started on the port http://localhost:8080'))
     } catch (error){
      console.log(error)
--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -11,7 +11,7 @@ const router=express.Router()
 
 <<<<<<< HEAD
 const configuration=new Configuration({
-    apiKey:process.env.OPENAI_API_KEY,
+    apikey:process.env.OPEN_API_KEY,
 })
 
 const openai=new OpenAIApi(configuration);
@@ -26,12 +26,12 @@ router.route('/').post(async(req,res)=>{
 
      const aiResponse=await openai.createImage({
         prompt,
-        n:1,
+        n:'1',
         size:'1024x1024',
         response_format:'b64_json',
      });
 
-     const image = aiResponse.data.data[0].b64_json;
+     const image=arResponse.data.data[0].b64_json;
      res.status(200).json({photo:image});
    }
    catch(error){
