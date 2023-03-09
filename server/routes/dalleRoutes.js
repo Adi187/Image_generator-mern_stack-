eaf2b867371456d2f4b8479524084b0dbfe8e2dc
Aditya sharma <adityaraysharma@gmail.com>
2023-03-09 23:25:04 +0530

    made correction in the api key

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -9,7 +9,7 @@ dotenv.config();
 const router=express.Router()
 
 const configuration=new Configuration({
-    apikey:process.env.OPEN_API_KEY,
+    apiKey:process.env.OPENAI_API_KEY,
 })
 
 const openai=new OpenAIApi(configuration);
@@ -24,12 +24,12 @@ router.route('/').post(async(req,res)=>{
 
      const aiResponse=await openai.createImage({
         prompt,
-        n:'1',
+        n:1,
         size:'1024x1024',
         response_format:'b64_json',
      });
 
-     const image=arResponse.data.data[0].b64_json;
+     const image = aiResponse.data.data[0].b64_json;
      res.status(200).json({photo:image});
    }
    catch(error){
